add208e0c0c3d0423beb819dcfbe836ae3c1cbc4
Yarin Tzdaka <yarintz33@gmail.com>
2025-09-03 12:45:35 +0300

    init client

---
+++ server/models/taskModel.js
@@ -0,0 +1,10 @@
+const taskModel = {
+  id: number,
+  title: string,
+  description: string,
+  completed: boolean,
+  createdAt: Date,
+  priority: string,
+};
+
+export default taskModel;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import tasksRoutes from "./routes/tasks.js";
 
 const app = express();
 const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(tasksRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
